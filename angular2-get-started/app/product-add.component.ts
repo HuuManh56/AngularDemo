2eed360d687254a3d627bc79246e03bc276d66c2
HuuManh56 <43846832+HuuManh56@users.noreply.github.com>
2019-07-08 17:00:26 +0700

    update product-add

--- angular2-get-started/app/product-add.component.ts
+++ angular2-get-started/app/product-add.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { listProduct } from './service/product.sevice';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CategoryService } from './service/category.service';
-import { randomBytes } from 'crypto';
-
+import { FormBuilder, FormGroup, Validators}from '@angular/forms'
 
 @Component({
     selector: 'product-add',
@@ -16,57 +13,39 @@ export class ProductAdd implements OnDestroy, OnInit {
     public _id: number;
     public subscription: any;
     public product: any;
-    public cate: any
     constructor(
         private router: Router,
         private fromBuider: FormBuilder,
         private activateRoute: ActivatedRoute,
-        public list: listProduct,
-        public categoryService: CategoryService,
+        public list: listProduct
     ) {
 
     }
     ngOnDestroy() {
-
+     
     }
     ngOnInit() {
-        this.formProduct = this.fromBuider.group({
+        this.formProduct=this.fromBuider.group({
             name: this.fromBuider.control('', Validators.required),
             price: this.fromBuider.control(''),
             describe: this.fromBuider.control(''),
             image: this.fromBuider.control(''),
             category: this.fromBuider.control('')
         });
-        this.product = {};
-
-        this.categoryService.GetCategory().subscribe((response: any) => {
-            this.cate = response;
-        })
-
+        this.product={};
+       
     }
 
-    SaveForm() {
-        this.list.Add(this.product).subscribe(responser => {
-            if (responser) {
+    SaveForm(){
+        this.list.Add(this.product).subscribe(responser=>{
+            if(responser){
                 alert(" save success");
                 this.router.navigate(['/product']);
             }
         })
     }
-    GotoListProduct() {
+    GotoListProduct(){
         this.router.navigate(['/product']);
     }
-
-    url: string;
-    onSelectFile(event) { // called each time file input changes
-        if (event.target.files && event.target.files[0]) {
-            var reader = new FileReader();
-
-            reader.readAsDataURL(event.target.files[0]); // read file as data url
-
-            reader.onload = (event) => { // called once readAsDataURL is completed
-                this.url = event.target.result;
-            }
-        }
-    }
+    
 }
